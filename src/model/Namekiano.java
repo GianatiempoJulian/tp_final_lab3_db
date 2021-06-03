f79832f893ada64d583ae0e8e952af923a174577
Federico Soler <federicosoler@live.com.ar>
2021-06-03 19:10:03 -0300

    cambios

--- src/model/Namekiano.java
+++ src/model/Namekiano.java
@@ -1,38 +1,73 @@
 package model;
 
-public class Namekiano extends Character{
-    private String nam_class;
-    private boolean nam_isFusion;
+public class Namekiano extends Character {
+	private String nam_type; ///tipo : comun o combatiente.
+	private boolean nam_fusion;
+	
+	public Namekiano() {
+		super();
+		nam_type = "comun";
+		nam_fusion =false;
+	}
+	public Namekiano(String char_name, double char_ki, boolean char_isAlive, String char_planet,String nam_type,boolean nam_fusion) {
+		super(char_name, char_ki, char_isAlive, char_planet);
+		this.nam_fusion = nam_fusion;
+		this.nam_type = nam_type;
+		
+	}
+	
+	
+	///***GETTERS***///
+	
+	public String getNam_type() {
+		return nam_type;
+	}
+	public boolean isNam_fusion() {
+		return nam_fusion;
+	}
+	
+	///***SETTERS***///
+	
+	public void setNam_fusion(boolean nam_fusion) {
+		this.nam_fusion = nam_fusion;
+	}
+	
+	///***METHODS***///
+	
+	@Override
+	public int hit_basic() {
+		int retorno = 100;
+		if(nam_type.equals("comun")) {
+			retorno = 10;
+		}
+		return retorno;
+	}
+	
+	@Override
+	public double hit_ki() {
+		int retorno = 200;
+		if(nam_type.equals("comun")) {
+			retorno = 0;
+		}
+		return retorno;
+	}
+	
+	public String nam_boolean_ToString() {
+		 String answer;
 
-    public Namekiano (){}
-
-    public Namekiano (String nam_class,boolean nam_isFusion){
-        super();
-        this.nam_class = nam_class;
-        this.nam_isFusion = nam_isFusion;
-    }
-
-    public Namekiano (String char_name, double char_ki, boolean char_isAlive, String char_planet,String nam_class,boolean nam_isFusion){
-        super(char_name,char_ki,char_isAlive,char_planet);
-        this.nam_class = nam_class;
-        this.nam_isFusion = nam_isFusion;
-    }
-
-    ///***GETTERS***///
-
-    public String getNam_class() {
-        return nam_class;
-    }
-
-    public boolean getNam_isFusion(){
-        return nam_isFusion;
-    }
-
-    ///***SETTERS***///
-
-    public void setNam_isFusion(boolean nam_isFusion) {
-        this.nam_isFusion = nam_isFusion;
-    }
-
-    
+	        if (nam_fusion)
+	        {
+	            answer = "Si";
+	        }
+	        else {
+	            answer = "No";
+	        }
+	        return answer;
+	}
+	@Override
+	public String toString() {
+		return super.toString()+"  Raza : Namekiano Tipo :" + nam_type + ", Esta fusionado: " + nam_boolean_ToString() ;
+	}
+	
+	
 }
