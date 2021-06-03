77ccc4b95cf28a4c3cad786dc1606ecbddf6a6d1
Federico Soler <federicosoler@live.com.ar>
2021-06-03 18:48:23 -0300

    cambio del main y algunas clases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,24 @@
 package com.company;
 
+import model.Androide;
 import model.Character;
+import model.Humano;
+import model.Namekiano;
 import model.Saiyan;
 
 public class Main {
 
     public static void main(String[] args) {
-    Character c1 = new Saiyan("Goku",720000,true,"vegita",7,true,false);
-    System.out.println(c1.toString());
+    	Namekiano dende = new Namekiano("Dende", 5, true, "Namek", "comun", false);
+    	System.out.println(dende.hit_basic());
+    	System.out.println(dende.hit_ki());
+    	System.out.println(dende.toString());
+    	
+    	Androide numero17 = new Androide("Numero17",10000 , true, "Tierra", "Dr Gero");
+    	System.out.println(numero17.toString());
+    	
+    	Humano krillin = new Humano("Krillin",75000 , true, "Tierra", true);
+    	System.out.println(krillin.toString());
     }
 }
+ 
