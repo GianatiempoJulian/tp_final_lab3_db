f7f8d5a245e5c5943eaf69ffc06c6b40c29f28c9
Julian Gianatiempo <gianatiempoattorney@icloud.com>
2021-06-04 15:22:50 -0300

    Interfaces agregadas

---
+++ src/interfaz/IAndroidAttacks.java
@@ -0,0 +1,10 @@
+package interfaz;
+
+public interface IAndroidAttacks {
+
+    public int barrage();
+    public int infinty_bullet();
+    public int power_wave();
+    public int absorb_power();
+
+}
