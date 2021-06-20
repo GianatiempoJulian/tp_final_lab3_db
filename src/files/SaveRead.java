4dfaa848a76d8a918d21a81b18a5499a72718155
Federico Soler <federicosoler@live.com.ar>
2021-06-20 16:53:22 -0300

    terminado los archivos

--- src/files/SaveRead.java
+++ src/files/SaveRead.java
@@ -1,19 +1,24 @@
 package files;
 
-import java.io.EOFException;
+import java.io.EOFException; 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+
+import model.CollectionPj;
+import model.User;
+import model.UserDataBase;
 
 public class SaveRead {
-		
-	
-	public static <T> void writeHash(HashSet<T> collection,File mf) {
+
+	public static <T> void writeHash(HashSet<T> collection, File mf) {
 		ObjectOutputStream salida = null;
 		try {
 			salida = new ObjectOutputStream(new FileOutputStream(mf));
@@ -35,7 +40,8 @@ public class SaveRead {
 			}
 		}
 	}
-	public static <T> void readHash(File mf) { 
+
+	public static <T> void readHash(File mf) {
 		ObjectInputStream entrada = null;
 		try {
 			boolean more = true;
@@ -62,33 +68,93 @@ public class SaveRead {
 				e2.printStackTrace();
 			}
 		}
-	} 
+	}
+
+	public static <T> void readHash2(File mf,CollectionPj<T> coleccion ) {
+		ObjectInputStream entrada2 = null;
+		try {
+			boolean more = true;
+			entrada2 = new ObjectInputStream(new FileInputStream(mf));
+
+			while (more) {
+				T element = (T) entrada2.readObject();
+				
+				coleccion.addElement(element);
+			}
+
+		} catch (EOFException e) {
+			
+		}
+
+		catch (IOException ef) {
+			System.out.println("Error en el archivo ");
+			ef.printStackTrace();
+		} catch (ClassNotFoundException er) {
+			System.out.println("No se encontro la clase");
+			er.printStackTrace();
+		} finally {
+			try {
+
+				entrada2.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	
+	public static void readHashMap(File fuser,UserDataBase coleccion) {
+		ObjectInputStream entrada3 = null;
+		try {
+			boolean more = true;
+			entrada3 = new ObjectInputStream(new FileInputStream(fuser));
+
+			while (more) {
+				User element = (User) entrada3.readObject();
+				coleccion.addUser(element);
+			}
+
+		} catch (EOFException e) {
+			System.out.println("Fin de lectura");
+		}
+
+		catch (IOException ef) {
+			System.out.println("Error en el archivo");
+		} catch (ClassNotFoundException er) {
+			System.out.println("No se encontro la clase");
+		} finally {
+			try {
+
+				entrada3.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+	public static void writeHashMap(HashMap<Integer,User> collection, File mf) {
+		ObjectOutputStream salida2 = null;
+		try {
+			salida2 = new ObjectOutputStream(new FileOutputStream(mf));
+
+			Iterator itrIterator = collection.entrySet().iterator();
+			while (itrIterator.hasNext()) {
+				Map.Entry me =(Map.Entry) itrIterator.next();
+		
+				salida2.writeObject(me);
+			}
+
+		} catch (IOException e) {
+			System.out.println("error en el archivo");
+
+		} finally {
+			try {
+				salida2.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //public void readHash() { /// esta sirve para escribir cuando hay una coleccion con datos ejecutandose al
 //	/// momento
@@ -120,4 +186,3 @@ public class SaveRead {
 //}
 //}
 //}
-
